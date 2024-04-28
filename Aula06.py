1aaacc7a66807efdf2c81e9445aa111d61000e02
Cauã Schunck <branquelinho.schunck@gmail.com>
2024-04-28 17:34:00 -0300

    Initial commit

---
+++ Aula06.py
@@ -0,0 +1,6 @@
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite outro: '))
+s = n1 + n2
+#print('A soma entre',n1, 'e', n2, 'é igual a:', s) A linha de baixo faz a mesma coisa, mas é melhor.
+print('A soma entre {} e {} vale {}'.format(n1, n2, s))
+
