1eb397ac740664d1d4278874feb95f438ae68c8a
Cauã Schunck <branquelinho.schunck@gmail.com>
2024-04-28 18:04:32 -0300

    mostrando os "is..." em python

---
+++ Aula6b.py
@@ -0,0 +1,24 @@
+nbool = bool(input('Digite um valor: '))
+print(n)
+
+#se colocar um valor ele vai responder TRUE se só apertar enter quando pedir um
+# valor ele vai responder FALSE porque ele indentifica quese tem um número é verdadeiro
+#----------------------------------------------------------------------------
+
+nNúmero = input('Digite um número: ')
+print(n.isnumeric())
+
+#aqui ele responde se o valor é um numeor ou não
+#----------------------------------------------------------------------------
+nLetra = input('Digite uma letra: ')
+print(n.isalpha())
+
+#verifica de o valor de N será letras
+#----------------------------------------------------------------------------
+
+nAlphaNúmerico = input('Digite um valor alpha númerico: ')
+print(n.isalnum())
+
+#verifica se o valor de N é alpha númerico
+#----------------------------------------------------------------------------
+
