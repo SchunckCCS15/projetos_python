c3771dd28ef58cd452561ccd83d0fe97d0a39501
Cauã Schunck <branquelinho.schunck@gmail.com>
2024-05-01 20:29:51 -0300

    atuaizações do curso

---
+++ Exercicio019.py
@@ -0,0 +1,6 @@
+import random
+
+nomes = 'Alice', 'Cauã', 'Célio', 'Asenate'
+escolha = random.choices(nomes)
+
+print('Já que ninguém se voluntariou eu vou escolher quem vai apagar a lousa e vai ser... {}!!!'.format(escolha))
